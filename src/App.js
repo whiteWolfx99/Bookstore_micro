9eea6eade2829d167b2e23017478d787690b6229
whiteWolfx99 <hhqu180099@uniq.edu.iq>
2022-12-09 22:17:29 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Math Magicians</h1>
+    </div>
+  );
+}
