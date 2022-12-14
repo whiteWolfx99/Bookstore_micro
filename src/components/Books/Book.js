0aa76f879f09d10eb7fa84ecb52e437654a12308
whiteWolfx99 <hhqu180099@uniq.edu.iq>
2022-12-14 15:53:52 +0300

    connect to API

--- src/components/Books/Book.js
+++ src/components/Books/Book.js
@@ -2,21 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Book({
-  title, author, handleRemoveBook,
+  book, handleRemoveBook, id,
 }) {
   return (
-    <div className="Book">
-      <h3>{title}</h3>
-      <p>{author}</p>
+    <li className="Book">
+      <h3>{book[0].title}</h3>
+      <p>{book[0].author}</p>
+      <p>{book[0].category}</p>
+      <p>{id}</p>
       <button type="button" onClick={handleRemoveBook}>Remove Book</button>
-    </div>
+    </li>
   );
 }
 
 Book.propTypes = {
-  title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
+  book: PropTypes.instanceOf(Array).isRequired,
   handleRemoveBook: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default Book;
--- src/components/Books/Booklist.js
+++ src/components/Books/Booklist.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeBook } from '../../redux/books/books';
+import { removeBookAsync, fetchBooks } from '../../redux/books/books';
 import Book from './Book';
 
 function Booklist() {
   const books = useSelector((state) => state.books.books);
   const dispatch = useDispatch();
 
-  const handleRemoveBook = (id) => {
-    dispatch(removeBook(id));
+  const handleRemoveBook = async (id) => {
+    await dispatch(removeBookAsync(id));
+    dispatch(fetchBooks());
   };
+
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
 
   return (
     <div>
-      {books.map((book) => (
-        <Book
-          key={book.id}
-          title={book.title}
-          author={book.author}
-          handleRemoveBook={() => handleRemoveBook(book.id)}
-        />
-      ))}
+      <ul key={books.item_id}>
+        {Object.keys(books).map((key) => (
+          <Book
+            key={key}
+            id={key}
+            book={books[key]}
+            handleRemoveBook={() => handleRemoveBook(key)}
+          />
+        ))}
+      </ul>
     </div>
   );
 }
