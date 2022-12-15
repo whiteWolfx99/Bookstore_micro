3008414e52c254c9e9470f7b7711ce9a3892a27b
whiteWolfx99 <hhqu180099@uniq.edu.iq>
2022-12-15 13:03:23 +0300

    Bookstore: styling

--- src/components/Books/Book.js
+++ src/components/Books/Book.js
@@ -1,24 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 function Book({
-  book, handleRemoveBook, id,
+  book, handleRemoveBook,
 }) {
+  const [progress, setProgress] = useState(0);
+
+  const handleProgress = () => {
+    if (progress < 100) {
+      setProgress(progress + 10);
+    }
+  };
+
   return (
-    <li className="Book">
-      <h3>{book[0].title}</h3>
-      <p>{book[0].author}</p>
-      <p>{book[0].category}</p>
-      <p>{id}</p>
-      <button type="button" onClick={handleRemoveBook}>Remove Book</button>
-    </li>
+    <div className="Book">
+      <div className="text-book">
+        <p className="category">{book[0].category}</p>
+        <h3 className="title">{book[0].title}</h3>
+        <span className="author">{book[0].author}</span>
+        <div className="function-button">
+          <button className="border-right-1" type="button">Comments</button>
+          <button className="border-right-2" type="button" onClick={handleRemoveBook}>Remove</button>
+          <button className="border-none" type="button">Edit</button>
+        </div>
+      </div>
+      <div className="flex-items">
+        <div className="progress-bar">
+          <div className="progress">
+            <CircularProgressbar value={progress} />
+          </div>
+          <div className="progress-percentage">
+            <p>
+              {progress}
+              %
+            </p>
+            <span>Completed</span>
+          </div>
+        </div>
+        <div className="progress-buttons">
+          <p>CURRENT CHAPTER</p>
+          <span>Chapter 17</span>
+          <button type="button" onClick={handleProgress}>UPDATE PROGRESS</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
 Book.propTypes = {
   book: PropTypes.instanceOf(Array).isRequired,
   handleRemoveBook: PropTypes.func.isRequired,
-  id: PropTypes.string.isRequired,
 };
 
 export default Book;
